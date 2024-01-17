e676f40c82d7a55038830ae617a49e3f5c798782
Brigette <long.brigettee@gmail.com>
2024-01-17 13:44:25 -0800

    Add player buttons to HTML file
    Creating player buttons eliminates need for players to type answers.
    Typed answers involving typos can result in errors.
    Code previously used to handle invalid inputs have been temporarily
    commented out.

--- main.js
+++ main.js
@@ -2,26 +2,26 @@ const choices = ["Rock", "Paper", "Scissors"];
 const win = "WIN!";
 const lose = "Lost :(";
 const tie = "Tie!";
-const notAChoice = "Please enter a valid response - rock, paper, or scissors."
+// const notAChoice = "Please enter a valid response - rock, paper, or scissors."
 var playerScore = 0;
 var computerScore = 0 
 
-for (let i = 0; i < 5; i++) {
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection === computerSelection) {
-            console.log(tie); 
-        } else if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") { 
-            console.log(notAChoice); 
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log(tie); 
+    //} else if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") { 
+      //  console.log(notAChoice); 
+    } else {
+        if (playerSelection === "scissors" && computerSelection === choices[1] || playerSelection === "rock" && computerSelection === choices[2] || playerSelection == "paper" && computerSelection === choices[0]) {
+            playerScore = playerScore++;
+            console.log(win);
         } else {
-            if (playerSelection === "scissors" && computerSelection === choices[1] || playerSelection === "rock" && computerSelection === choices[2] || playerSelection == "paper" && computerSelection === choices[0]) {
-                playerScore = playerScore++;
-                console.log(win);
-            } else {
-                computerScore = computerScore++;
-                console.log(lose);
-            }
+            computerScore = computerScore++;
+            console.log(lose);
         }
     }
+
     let playerSelection = prompt("Enter rock, paper, or scissors").toLowerCase();
     let computerSelection = choices[Math.floor(Math.random() * choices.length)];
     console.log(playRound(playerSelection, computerSelection))
